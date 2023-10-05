6b443b568df1e89840ad51b791d070eff12aff66
Yuan-San <yuan.arib@gmail.com>
2023-10-05 19:48:17 +0700

    lmao

--- ipt/wsgi.py
+++ ipt/wsgi.py
@@ -6,11 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
-
 import os
+import sys
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ipt.ipt.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ipt.ipt.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ipt.settings')
 
 application = get_wsgi_application()
